db1a96e4ccc7344fbd4a01db82ec81f027cc5616
Nguyen Thanh An <thanhan030301@gmail.com>
2023-12-24 23:07:41 +0700

    4.2

--- _4/src/main.rs
+++ _4/src/main.rs
@@ -1,9 +1,12 @@
+#![allow(dead_code)]
+
 use std::collections::HashSet;
 
 fn main() {
     let input = std::fs::read_to_string("./input.txt").unwrap();
 
-    let result = part_1(&input);
+    // let result = part_1(&input);
+    let result = part_2(&input);
 
     println!("{:?}", result);
 }
@@ -15,6 +18,26 @@ fn part_1(input: &str) -> u32 {
         .filter(|&x| x > 0)
         .map(|x| 2_u32.pow((x as u32) - 1))
         .sum()
+}
+
+fn part_2(input: &str) -> u32 {
+    let ms = input
+        .lines()
+        .map(|s| count_(split_card(s).unwrap()))
+        .collect::<Vec<usize>>();
+
+    let mut cs = Vec::with_capacity(ms.len());
+    cs.push(1);
+
+    for ci in 1..ms.len() {
+        let mut result = 0;
+        for j in 0..ci {
+            result += (if ms[j] >= (ci - j) { 1 } else { 0 }) * cs[j];
+        }
+        cs.push(result + 1);
+    }
+
+    cs.into_iter().sum()
 }
 
 fn split_card(input: &str) -> Option<(&str, &str)> {
